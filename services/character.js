044c24a68652f90b55e8e46bcb411d6580603453
Tyrone Williams <itenory@gmail.com>
2019-03-09 17:37:37 -0500

    Created function for getting characters by name

--- services/character.js
+++ services/character.js
@@ -9,18 +9,34 @@ exports.createCharacter = (discordId, name, job) => {
   return Character({
     discordId,
     name,
+    displayName: name,
     job
   }).save();
 };
 
 /**
- * Deletes a character by the user's discord id.
- * @param {string} discordId Discord id of user removing character.
+ * Deletes a character by the user's discord id and name.
+ * @param {string} discordId Discord id of user deleting character
  * @param {string} name Name of character
  * @return {Promise<Object>} A promise to resolve with the deleted character.
  */
 exports.deleteCharacter = (discordId, name) => {
   return Character.findOneAndDelete({
+    discordId,
+    displayName: name,
+    name
+  }).exec();
+};
+
+/**
+ * Finds and returns a character by name and discordId.
+ * @param {string} discordId Discord id of the user the character belongs to
+ * @param {string} name Name of character to find
+ * @return {Promise<object>} A promise to resolve with a character object, or
+ *  null if no character could be found.
+ */
+exports.getCharacterByName = (discordId, name) => {
+  return Character.findOne({
     discordId,
     name
   }).exec();
