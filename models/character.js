949e9af9ffebc1fe243b75123a0b564df80b2111
Tyrone Williams <itenory@gmail.com>
2019-03-09 17:38:04 -0500

    Created displayName field

--- models/character.js
+++ models/character.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
   discordId: { type: String, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true, lowercase: true },
+  displayName: { type: String, required: true },
   job: { type: String, required: true },
   level: { type: Number, default: 1 },
   created: { type: Date, default: Date.now }
