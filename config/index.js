1f6df7ac062836ff528b6f07200c4fb22756cde6
williamstyronejr <itenory@gmail.com>
2020-01-01 15:13:59 -0500

    Removed unused files

--- config/index.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./config.prod');
-} else if (process.env.NODE_ENV === 'ci') {
-  module.exports = require('./config.ci');
-} else {
-  module.exports = require('./config.dev');
-}
--- config/stub.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  botTokens: {
-    // List of bot names with respective tokens.
-    SAMPLE_NAME: ''
-  },
-  mongoURI: '' // URI for mongo
-};
